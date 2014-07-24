d99fc190f718f6e704dfd98eda2df81ffce04467
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2014-07-24 13:41:37 +0200

    Added button to call Feedly Cloud

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,10 @@
-function doClick(e) {
-    alert($.label.text);
+var TiFeedly = require('tifeedly');
+
+
+function startFeedly(e) {
+    var feedly = new TiFeedly('sandbox','ES3R6KCEG46BW9MYD332');
+    feedly.login();
 }
+
 
 $.index.open();
